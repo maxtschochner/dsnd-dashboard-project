20a5d4515cebb338be044353f376ce9126badd66
Maximilian Tschochner <maximilian.tschochner@gmail.com>
2025-03-28 21:12:28 +0100

    improved flake8 linting

--- tests/test_employee_events.py
+++ tests/test_employee_events.py
@@ -6,15 +6,18 @@ from pathlib import Path
 # for the root of this project
 project_root = Path(__file__).parents[1]
 
+
 # apply the pytest fixture decorator
 # to a `db_path` function
 @pytest.fixture
 def db_path():
-    
+
     # Using the `project_root` variable
     # return a pathlib object for the `employee_events.db` file
-    path_to_db = (project_root / 'python-package' / 'employee_events' / 'employee_events.db')
+    path_to_db = (project_root / 'python-package'
+                  / 'employee_events' / 'employee_events.db')
     return path_to_db
+
 
 # Define a function called
 # `test_db_exists`
@@ -23,21 +26,26 @@ def db_path():
 # the creates the "fixture" for
 # the database's filepath
 def test_db_exists(db_path):
-    
+
     # using the pathlib `.is_file` method
     # assert that the sqlite database file exists
     # at the location passed to the test_db_exists function
     assert db_path.is_file()
+
 
 @pytest.fixture
 def db_conn(db_path):
     from sqlite3 import connect
     return connect(db_path)
 
+
 @pytest.fixture
 def table_names(db_conn):
-    name_tuples = db_conn.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+    name_tuples = (db_conn.execute
+                   ("SELECT name FROM sqlite_master WHERE type='table';")
+                   .fetchall())
     return [x[0] for x in name_tuples]
+
 
 # Define a test function called
 # `test_employee_table_exists`
@@ -49,6 +57,7 @@ def test_employee_table_exists(table_names):
     # is in the table_names list
     assert 'employee' in table_names
 
+
 # Define a test function called
 # `test_team_table_exists`
 # This function should receive the `table_names`
@@ -57,7 +66,8 @@ def test_team_table_exists(table_names):
 
     # Assert that the string 'team'
     # is in the table_names list
-    assert 'tea' in table_names
+    assert 'team' in table_names
+
 
 # Define a test function called
 # `test_employee_events_table_exists`
@@ -68,4 +78,3 @@ def test_employee_events_table_exists(table_names):
     # Assert that the string 'employee_events'
     # is in the table_names list
     assert 'employee_events' in table_names
-
